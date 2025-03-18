66017e936c1a169a5b085e6ff1d64a32e617e441
prathum08 <prathumbhangadia08@gmail.com>
2025-03-18 23:14:50 +0530

    Home Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { LoadingScreen } from './components/LoadingScreen';
 import { useEffect, useState } from "react";
 import { Navbar } from './components/Navbar';
 import { MobileMenu } from './components/MobileMenu';
+import { Home } from './components/sections/Home';
 import "./index.css";
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
 
       <Navbar menuOpen = {menuOpen} setMenuOpen = {setMenuOpen} />
       <MobileMenu menuOpen = {menuOpen} setMenuOpen = {setMenuOpen} />
+      <Home />
 
     </div>
 
