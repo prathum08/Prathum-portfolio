0be5605a55475e06eb3e16d8963673232564178a
prathum08 <prathumbhangadia08@gmail.com>
2025-03-19 14:18:12 +0530

    about page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { Navbar } from './components/Navbar';
 import { MobileMenu } from './components/MobileMenu';
 import { Home } from './components/sections/Home';
+import { About } from './components/sections/About';
 import "./index.css";
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
       <Navbar menuOpen = {menuOpen} setMenuOpen = {setMenuOpen} />
       <MobileMenu menuOpen = {menuOpen} setMenuOpen = {setMenuOpen} />
       <Home />
+      <About />
 
     </div>
 
