a6dc1078568dc3141ffb606f133083bc26ab52ec
prathum08 <prathumbhangadia08@gmail.com>
2025-03-22 19:24:45 +0530

    u

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -6,7 +6,7 @@ export const Home = () =>{
         <RevealOnScroll>
 
         <div className="text-center z-10 px-4">
-            <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent leading-right">Hi, I'm Prathum Bhangadia
+            <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent leading-right">H, I'm Prathum Bhangadia
 
             </h1>
 
