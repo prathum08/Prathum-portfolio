3c5829ae109fe279714998b7781b4f1c514c3d82
prathum08 <prathumbhangadia08@gmail.com>
2025-03-22 19:08:10 +0530

    hosting update

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -33,7 +33,7 @@ export const Contact = () => {
       className="min-h-screen flex items-center justify-center py-28"
     >
       <RevealOnScroll>
-        <div className="px-4 w-150">
+        <div className="px-4 w-full md:w-150">
           <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
             Get In Touch
           </h2>
--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,5 @@ export default defineConfig({
   plugins: [react(),
     tailwindcss(),
   ],
+  base:"Prathum-portfolio",
 })
