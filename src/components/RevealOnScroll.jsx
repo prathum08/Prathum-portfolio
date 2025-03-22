b94301c7b2e219b7b7e3ebe3e87af5134dde7de6
prathum08 <prathumbhangadia08@gmail.com>
2025-03-22 15:25:02 +0530

    Scroll Animation

--- src/components/RevealOnScroll.jsx
+++ src/components/RevealOnScroll.jsx
@@ -1,6 +1,26 @@
+import { useRef, useEffect } from "react";
 
 
-const RevealOnScroll = ({ children}) => {
+export const RevealOnScroll = ({ children }) => {
+    
+    const ref = useRef(null);
+    useEffect(() =>{
+        const observer = new IntersectionObserver(([entry]) =>{
+            if (entry.isIntersecting) {
+                ref.current.classList.add("visible");
+            } else {
+                ref.current.classList.remove("visible");
+            }
+        } , {threshold: 0.2 , rootMargin: "0px 0px -50px 0px"});
 
-    return <div> {children} </div>
+        if(ref.current) observer.observe(ref.current);
+
+        return () => observer.disconnect();
+    });
+
+    return (
+        <div ref = {ref} className="reveal"> 
+            {children} 
+        </div>
+    );
 };
--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -1,7 +1,9 @@
+import { RevealOnScroll } from "../RevealOnScroll";
 
 export const Home = () =>{
     return <section id="home" className="min-h-screen flex items-center justify-center relative">
-
+        
+        <RevealOnScroll>
 
         <div className="text-center z-10 px-4">
             <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent leading-right">Hi , I'm Prathum Bhangadia
@@ -21,5 +23,6 @@ export const Home = () =>{
                 
             </div>
         </div>
+        </RevealOnScroll>
     </section>
 }
